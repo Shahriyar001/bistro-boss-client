6a5f86d705c742c765b7fed863d4c7b2ceb899b5
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-03-12 23:14:33 +0600

    added all routes by name in sidebar of dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -12,7 +12,38 @@ const Dashboard = () => {
     <div className="flex">
       {/* dashboard sidebar  */}
       <div className="w-64 min-h-screen bg-orange-400">
-        <ul className="menu"></ul>
+        <ul className="menu">
+          <li>
+            <NavLink to="/dashboard/userHome">
+              {" "}
+              <FaHome /> User Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/reservation">
+              {" "}
+              <FaCalendar /> Reservation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/cart">
+              {" "}
+              <FaShoppingCart /> My Cart
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/review">
+              {" "}
+              <FaAd /> Add a Review
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/bookings">
+              {" "}
+              <FaList /> My Bookings
+            </NavLink>
+          </li>
+        </ul>
       </div>
       {/* dashboard content  */}
       <div className="flex-1">
