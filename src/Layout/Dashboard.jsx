3b0407352a131ceed797b99bb9ca8df74e7d0e8b
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-03-15 23:44:36 +0600

    change sidebar for admin

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,51 +1,97 @@
 import {
   FaAd,
+  FaBook,
   FaCalendar,
+  FaEnvelope,
   FaHome,
   FaList,
   FaSearch,
   FaShoppingCart,
+  FaUsers,
+  FaUtensils,
 } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
   const [cart] = useCart();
+
+  // TODO: get isadmin value from the database
+  const isAdmin = true;
+
   return (
     <div className="flex">
       {/* dashboard sidebar  */}
       <div className="w-64 min-h-screen bg-blue-400 text-black">
         <ul className="menu">
-          <li>
-            <NavLink to="/dashboard/userHome">
-              {" "}
-              <FaHome /> User Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/reservation">
-              {" "}
-              <FaCalendar /> Reservation
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/cart">
-              {" "}
-              <FaShoppingCart /> My Cart ({cart.length})
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/review">
-              {" "}
-              <FaAd /> Add a Review
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/bookings">
-              {" "}
-              <FaList /> My Bookings
-            </NavLink>
-          </li>
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink to="/dashboard/adminHome">
+                  {" "}
+                  <FaHome /> Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/addItems">
+                  {" "}
+                  <FaUtensils /> Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manageItems">
+                  {" "}
+                  <FaList /> Manage Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/bookings">
+                  {" "}
+                  <FaBook /> Manage Bookings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/users">
+                  {" "}
+                  <FaUsers /> All Users
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/dashboard/userHome">
+                  {" "}
+                  <FaHome /> User Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/reservation">
+                  {" "}
+                  <FaCalendar /> Reservation
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/cart">
+                  {" "}
+                  <FaShoppingCart /> My Cart ({cart.length})
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/review">
+                  {" "}
+                  <FaAd /> Add a Review
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/bookings">
+                  {" "}
+                  <FaList /> My Bookings
+                </NavLink>
+              </li>
+            </>
+          )}
+          {/* shared nav links  */}
           <div className="divider"></div>
           <li>
             <NavLink to="/">
@@ -60,9 +106,9 @@ const Dashboard = () => {
             </NavLink>
           </li>
           <li>
-            <NavLink to="/">
+            <NavLink to="/order/contact">
               {" "}
-              <FaHome /> User Home
+              <FaEnvelope /> Contact
             </NavLink>
           </li>
         </ul>
