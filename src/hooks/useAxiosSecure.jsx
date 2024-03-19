d229e3171a6602746859ac01e8e583a488ebc7e0
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-03-19 21:46:16 +0600

    Verify Token And Axios Interceptor

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,6 +5,30 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      console.log("request stooped by interceptors", token);
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  // intercepts 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    (error) => {
+      const status = error.response.status;
+      console.log("status error in the interceptor", status);
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
