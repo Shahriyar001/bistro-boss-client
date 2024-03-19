95ab1a13ac0be5cd5aa1821255e32ca195a17324
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-03-19 23:44:51 +0600

    Logout Unauthorized Access And Check Is Admin

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -12,7 +12,7 @@ const useAxiosSecure = () => {
   axiosSecure.interceptors.request.use(
     function (config) {
       const token = localStorage.getItem("access-token");
-      console.log("request stooped by interceptors", token);
+      // console.log("request stooped by interceptors", token);
       config.headers.authorization = `Bearer ${token}`;
       return config;
     },
@@ -28,7 +28,8 @@ const useAxiosSecure = () => {
     },
     async (error) => {
       const status = error.response.status;
-      console.log("status error in the interceptor", status);
+      // console.log("status error in the interceptor", status);
+      // for 401 and 403 logout the user and and move the user to the login
       if (status === 401 || status === 403) {
         await logOut();
         navigate("/login");
