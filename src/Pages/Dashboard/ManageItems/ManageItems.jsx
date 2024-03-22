5050092d055b7bcf3dfe216c9fde9cda73c8b309
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-03-22 15:17:00 +0600

    manage item section

---
+++ src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -0,0 +1,70 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
+
+const ManageItems = () => {
+  const [menu] = useMenu();
+  return (
+    <div>
+      <SectionTitle
+        heading="Manage All Items"
+        subHeading="Hurry up"
+      ></SectionTitle>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Item Name</th>
+                <th>Price</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src="/tailwind-css-component-profile-2@56w.png"
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">Hart Hagerty</div>
+                      <div className="text-sm opacity-50">United States</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  Zemlak, Daniel and Leannon
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Desktop Support Technician
+                  </span>
+                </td>
+                <td>Purple</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ManageItems;
